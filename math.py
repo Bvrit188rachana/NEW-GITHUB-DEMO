6668f05410fa46392605a029b24180b2834e74b8
Bvrit188rachana <62056168+Bvrit188rachana@users.noreply.github.com>
2021-07-02 14:20:54 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,21 @@
+#Add Implementation
+def add(x,y):
+    return x+y
+
+#subtract implementation    
+def subtract(x,y):
+    return x-y
+  
+#Multiply Implementation  
+def multiply(x,y):
+    return x*y
+
+#Divide Implementation    
+def divide(x,y):
+
+    if(y==0):
+        return "zero"
+    else:
+        return x/y
+def sqaure():
+    pass
